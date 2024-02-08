8c67f1d99854b0cd7aa6bfc07bcd5fee80b83885
Dan Garland <dan@organise.network>
2024-02-08 10:04:10 +0000

    Allow Identification to store country related values

--- lib/amplitude_api/identification.rb
+++ lib/amplitude_api/identification.rb
@@ -12,16 +12,32 @@ class AmplitudeAPI
     # @!attribute [ rw ] user_properties
     #   @return [ String ] the user_properties to be attached to the Amplitude Identify
     attr_accessor :user_properties
+    # @!attribute [ rw ] country
+    #   @return [ String ] the country to be sent to the Amplitude Identify
+    attr_accessor :country
+    # @!attribute [ rw ] region
+    #   @return [ String ] the region to be sent to the Amplitude Identify
+    attr_accessor :region
+    # @!attribute [ rw ] city
+    #   @return [ String ] the city to be sent to the Amplitude Identify
+    attr_accessor :city
+    # @!attribute [ rw ] dma
+    #   @return [ String ] the dma to be sent to the Amplitude Identify
+    attr_writer :dma
 
     # Create a new Identification
     #
     # @param [ String ] user_id a user_id to associate with the identification
     # @param [ String ] device_id a device_id to associate with the identification
     # @param [ Hash ] user_properties various properties to attach to the user identification
-    def initialize(user_id: "", device_id: nil, user_properties: {})
+    def initialize(user_id: "", device_id: nil, user_properties: {}, country: nil, region: nil, city: nil, dma: nil)
       self.user_id = user_id
       self.device_id = device_id if device_id
       self.user_properties = user_properties
+      self.country = country
+      self.region = region
+      self.city = city
+      self.dma = dma
     end
 
     def user_id=(value)
@@ -39,7 +55,11 @@ class AmplitudeAPI
     def to_hash
       {
         user_id: user_id,
-        user_properties: user_properties
+        user_properties: user_properties,
+        country: country,
+        region: region,
+        city: city,
+        dma: dma
       }.tap { |hsh| hsh[:device_id] = device_id if device_id }
     end
 
@@ -53,5 +73,17 @@ class AmplitudeAPI
         false
       end
     end
+
+    private
+
+    # I don't really know what DMA is, but I don't think we need it. However if we don't send a value, it resets the others. So if we have country, region and city but no DMA let's send them an empty string
+    def dma
+      if @dma == nil && country && region && city
+        ''
+      else
+        @dma
+      end
+    end
+
   end
 end
--- spec/lib/amplitude_api/identification_spec.rb
+++ spec/lib/amplitude_api/identification_spec.rb
@@ -3,55 +3,72 @@
 require "spec_helper"
 
 describe AmplitudeAPI::Identification do
-  user = Struct.new(:id)
+  subject(:identification) { described_class.new(**identification_params) }
+  let(:user) {  Struct.new(:id) }
 
   context "with a user object" do
+    let(:identification_params) {  { user_id: user.new(123) } }
+
     describe "#body" do
       it "populates with the user's id" do
-        identification = described_class.new(user_id: user.new(123))
         expect(identification.to_hash[:user_id]).to eq(123)
       end
     end
   end
 
   context "with a user id" do
+    let(:identification_params) {  { user_id: 123 } }
+
     describe "#body" do
       it "populates with the user's id" do
-        identification = described_class.new(user_id: 123)
         expect(identification.to_hash[:user_id]).to eq(123)
+      end
+
+      it "includes the device id" do
+        identification = described_class.new(user_id: 123, device_id: "abc")
+        expect(identification.to_hash[:device_id]).to eq("abc")
+      end
+
+      it "includes arbitrary user properties" do
+        identification = described_class.new(
+          user_id: 123,
+          user_properties: {
+            first_name: "John",
+            last_name: "Doe"
+          }
+        )
+        expect(identification.to_hash[:user_properties]).to eq(first_name: "John", last_name: "Doe")
+      end
+
+      context "with country data" do
+        let(:identification_params) { { user_id: 123, country: 'GB', region: 'Sussex', city: 'Lewes', dma: nil } }
+
+        it "includes country data" do
+          expect(identification.to_hash[:country]).to eq('GB')
+        end
+
+        it "includes region data" do
+          expect(identification.to_hash[:region]).to eq('Sussex')
+        end
+
+        it "includes city data" do
+          expect(identification.to_hash[:city]).to eq('Lewes')
+        end
+
+        it "includes dma data" do
+          expect(identification.to_hash[:dma]).to eq('')
+        end
       end
     end
   end
 
   context "without a user" do
+    let(:identification_params) {  { user_id: nil } }
+
     describe "#body" do
       it "populates with the unknown user" do
-        identification = described_class.new(user_id: nil)
         expect(identification.to_hash[:user_id]).to eq(AmplitudeAPI::USER_WITH_NO_ACCOUNT)
       end
-    end
-  end
-
-  describe "#body" do
-    it "includes the user id" do
-      identification = described_class.new(user_id: 123)
-      expect(identification.to_hash[:user_id]).to eq(123)
-    end
-
-    it "includes the device id" do
-      identification = described_class.new(user_id: 123, device_id: "abc")
-      expect(identification.to_hash[:device_id]).to eq("abc")
-    end
-
-    it "includes arbitrary user properties" do
-      identification = described_class.new(
-        user_id: 123,
-        user_properties: {
-          first_name: "John",
-          last_name: "Doe"
-        }
-      )
-      expect(identification.to_hash[:user_properties]).to eq(first_name: "John", last_name: "Doe")
     end
   end
 end
